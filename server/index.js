c7c347bfd612aca3df26f08c2dd2a620a6b23487
LuiiVasquez <luivasquez95@gmail.com>
2022-11-26 15:17:51 -0600

    using tasks routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+
 import { PORT } from "./config.js";
 
 const app = express();
+app.use(morgan('dev'))
 
 app.use(indexRoutes);
+app.use(tasksRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
