167837cc26e5a0c7c3849f0f724a8ab71d0c07f1
LuiiVasquez <luivasquez95@gmail.com>
2022-11-26 15:18:54 -0600

    creating tasks controllers

---
+++ server/controllers/tasks.controllers.js
@@ -0,0 +1,19 @@
+export const getTasks = (req, res) => {
+  res.send("Getting tasks");
+};
+
+export const getTask = (req, res) => {
+  res.send("Getting task");
+};
+
+export const createTask = (req, res) => {
+  res.send("Creating tasks");
+};
+
+export const updateTask = (req, res) => {
+  res.send("Updating tasks");
+};
+
+export const deleteTask = (req, res) => {
+  res.send("Deleting tasks");
+};
