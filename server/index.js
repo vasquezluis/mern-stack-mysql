2c0a9d973c560880acad2ab5d090dfcae3f948d9
LuiiVasquez <luivasquez95@gmail.com>
2022-11-27 12:25:14 -0600

    Adding cors origin

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+
 import indexRoutes from "./routes/index.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
 
 import { PORT } from "./config.js";
 
 const app = express();
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.use(indexRoutes);
 app.use(tasksRoutes);
