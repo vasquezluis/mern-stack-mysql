0f00ce36d822cc1c18f06fed331502ba3d9e2ecf
LuiiVasquez <luivasquez95@gmail.com>
2022-11-28 14:14:27 -0600

    adding delete tasks request

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 // traer tarea del servidor
 export const getTasksRequest = async () =>
-// obtener datos
+  // obtener datos
   await axios.get("http://localhost:4000/tasks");
 
 // objeto tarea traida desde el backend
 export const createTaskRequest = async (task) =>
   // enviar objeto task
   await axios.post("http://localhost:4000/tasks", task);
+
+export const deleteTaskRequest = async (id) =>
+  await axios.delete(`http://localhost:4000/tasks/${id}`);
