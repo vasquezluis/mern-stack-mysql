e3f1044570e6688d9c851520b3feae1e3f5fe068
LuiiVasquez <luivasquez95@gmail.com>
2022-11-28 13:27:18 -0600

    adding GET request from api

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+// traer tarea del servidor
+export const getTasksRequest = async () =>
+// obtener datos
+  await axios.get("http://localhost:4000/tasks");
+
 // objeto tarea traida desde el backend
 export const createTaskRequest = async (task) =>
   // enviar objeto task
