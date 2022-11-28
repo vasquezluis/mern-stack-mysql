c8058db50c1ffd4ec62007ec1631e1a882d6a636
LuiiVasquez <luivasquez95@gmail.com>
2022-11-28 17:05:21 -0600

    Creating taskcontextprovider tag

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,20 @@ import TasksPage from "./pages/TasksPage";
 import TasksForm from "./pages/TasksForm";
 import NotFound from "./pages/NotFound";
 
-import Navbar from "./pages/components/Navbar";
+import Navbar from "./components/Navbar";
+
+import { TaskContextProvider } from "./context/TaskProvider";
 
 function App() {
   return (
-    <>
+    <TaskContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<TasksPage />} />
         <Route path="/tasksform" element={<TasksForm />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </TaskContextProvider>
   );
 }
 
