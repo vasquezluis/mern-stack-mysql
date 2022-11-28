8aba28c90a39e5e6ddfa8f8a573ed310bfbea750
LuiiVasquez <luivasquez95@gmail.com>
2022-11-28 17:19:10 -0600

    removing useState and loadTasks function to taskprovider

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
-import { getTasksRequest } from "../api/tasks.api";
+import { useEffect } from "react";
 
-import TaskCard from "./components/TaskCard";
+import TaskCard from "../components/TaskCard";
+
+import { useTasks } from "../context/TaskProvider";
 
 function TasksPage() {
-  const [tasks, setTasks] = useState([]);
+
+  const {tasks, loadTasks} = useTasks()
 
   useEffect(() => {
-    // funcion que ejecuta la funcion get desde api
-    async function loadTasks() {
-      const response = await getTasksRequest();
-      setTasks(response.data);
-    }
     loadTasks();
   }, []);
 
