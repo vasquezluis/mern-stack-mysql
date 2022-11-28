ab05c18cb3fab659f9317ed80091b0bece96b432
LuiiVasquez <luivasquez95@gmail.com>
2022-11-28 13:28:42 -0600

    Listing tasks from taskcard and get api

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+
+import TaskCard from "./components/TaskCard";
 
 function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // funcion que ejecuta la funcion get desde api
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
   return (
-    <div>TasksPage</div>
-  )
+    <div>
+      <h1>Tasks</h1>
+      // recorrer setTasks (variable cond ata)
+      {tasks.map((task) => (
+        // crear task card desde TaskCard y enviarle como parametro el task que se esta recorriendo
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
+  );
 }
 
-export default TasksPage
+export default TasksPage;
